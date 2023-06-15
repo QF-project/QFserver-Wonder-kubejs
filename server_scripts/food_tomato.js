82f8c3d8b3b8bd4e6c49ef636a8d433b76ce5f9a
QianF <576765349@qq.com>
2023-06-15 21:40:15 +0800

    no message

--- server_scripts/food_tomato.js
+++ server_scripts/food_tomato.js
@@ -4,7 +4,6 @@ onEvent('item.tags', event => {
 	event.get('kubejs:tomato').add('croptopia:tomato')
 	event.get('kubejs:tomato').add('culinaire:tomato')
 	event.get('kubejs:tomato').add('farmersdelight:tomato')
-	event.get('kubejs:tomato').add('hamncheese:tomato')
 	event.get('kubejs:tomato').add('sandwichable:tomato')
 })
 
@@ -12,6 +11,5 @@ onEvent('recipes', event => {
 	event.replaceInput({}, "croptopia:tomato", "#kubejs:tomato")
 	event.replaceInput({}, "culinaire:tomato", "#kubejs:tomato")
 	event.replaceInput({}, "farmersdelight:tomato", "#kubejs:tomato")
-	event.replaceInput({}, "hamncheese:tomato", "#kubejs:tomato")
 	event.replaceInput({}, "sandwichable:tomato", "#kubejs:tomato")
 })
